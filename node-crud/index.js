d465f3cd779e5c5bf3f08b80c271762098e1c739
Benjamin Denom <ben.denom@gmail.com>
2020-11-11 22:52:42 +0100

    fork node process to use all cores

--- node-crud/index.js
+++ node-crud/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient;
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
 function startMongo() {
     const url = 'mongodb://localhost:27017';
@@ -22,32 +24,47 @@ function startMongo() {
     });
 }
 
-function startServer(args) {
-    const app = express();
+function startServer(app, args) {
     const port = 9002;
     
-    const mongoClient = args['client'];
-    const mongoColl = args['database'].collection('users');
+    // const mongoClient = args['client'];
+    // const mongoColl = args['database'].collection('users');
     
     app.get('/ping', (req, res) => {
         res.send('');
     });
 
-    app.get('/users', (req, res) => {
-        mongoColl.find({}).toArray((err, docs) => {
-            res.send(docs);
-        });
-    });
+    // app.get('/users', (req, res) => {
+    //     mongoColl.find({}).toArray((err, docs) => {
+    //         res.send(docs);
+    //     });
+    // });
     
-    const server = app.listen(port, () => {
+    app.listen(port, () => {
         console.log(`Server node listening on port ${port}.`);
     });
 
-    process.on('SIGTERM', () => {
-        mongoClient.close();
-        server.close();
-    });
+    // process.on('SIGTERM', () => {
+    //     mongoClient.close();
+    //     server.close();
+    // });
 }
 
-startMongo()
-    .then(startServer)
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
+    });
+} else {
+    const app = express();
+    app.get('/ping', (req, res) => {
+        res.send('');
+    });
+    app.listen(9002);
+    console.log(`Worker ${process.pid} started`);
+}
